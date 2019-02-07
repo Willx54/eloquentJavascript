59fea5e89ca7545a274b20c63c0c604ae17ad93a
Will <w_priddle@hotmail.com>
2019-02-07 16:29:02 +0000

    work in progress

---
+++ chapter4/scripts.js
@@ -0,0 +1,59 @@
+// The sum of a Range
+// const range = (start, end) => {
+//     let rangeArray = [];
+//     for (let i = start; i < end + 1;i++) {
+//         rangeArray.push(i);
+//     }
+//     return rangeArray
+// }
+
+// const sum = (numArray) => {
+//     console.log(numArray);
+//     let total = 0;
+//     for (let item in numArray) {
+//         total = total + numArray[item];
+//     }
+//     return total;
+// }
+
+// console.log(sum(range(1,10)));
+
+// Bonus
+// const range = (start, end, step = 1) => {
+//     let rangeArray = [];
+
+//     if (step < 0) {
+//         for (let i = start; i > end - 1;) {
+//             rangeArray.push(i);
+//             console.log(i, step)
+//             i = i + step;
+//         }
+//     } else {
+//         for (let i = start; i < end + 1;) {
+//             rangeArray.push(i);
+//             console.log(i, step)
+//             i = i + step;
+//         }
+//     }
+
+//     return rangeArray;
+// }
+
+// const sum = (numArray) => {
+//     // console.log(numArray);
+//     let total = 0;
+//     for (let value of numArray) {
+//         console.log(value);
+//         total = total + value;
+//     }
+//     return total;
+// }
+
+// funciton sum()
+
+// // console.log(sum(range(1,10)));
+// // console.log(range(1,10,2));
+// console.log(sum([1,1,2,1]));
+
+// Reversing an array
+
